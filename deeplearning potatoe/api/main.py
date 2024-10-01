a83116ef795d346414ced33efcb97995111d5d25
richarduk99 <richardselvarajni@gmail.com>
2024-10-01 21:20:58 +0100

    Add files via upload
    Project: Potato Disease Detection App
    
    Description: Developed a machine learning application to detect potato diseases using image classification.
    
    Key Responsibilities:
    
    Model Development: Designed and implemented a Convolutional Neural Network (CNN) for image classification to accurately identify various potato diseases.
    API Development: Utilized FastAPI and Uvicorn to create a robust and scalable API for serving the machine learning model.
    Deployment: Integrated MLOps practices to ensure continuous integration and deployment of the model, maintaining high availability and performance.
    Collaboration: Worked closely with a DevOps student to deploy the application on a cloud platform, ensuring seamless integration and operational efficiency.
    Technologies Used: Python, TensorFlow/Keras, FastAPI, Uvicorn, Docker, GCP

---
+++ deeplearning potatoe/api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI,File,UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model(r"D:\codes\deeplearning potatoe\models\1.h5")
+CLASS_NAMES = ["EARLY BLIGHT","LATE BLIGHT","HEALTHY"]
+
+@app.get("/ping")
+async def ping():
+    return {"message": "hello, I'm alive mahoraga"}
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file : UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image,axis=0)
+
+
+    prediction = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
